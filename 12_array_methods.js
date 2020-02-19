87c9bc7759f4b41da7f59ca20f1904be30b8f9a7
Nick Stelmakh <nutellaondream@gmail.com>
2020-02-19 13:44:03 +0200

    12.Array. Array Methods

---
+++ 12_array_methods.js
@@ -0,0 +1,43 @@
+const people = [{
+        name: 'Nick',
+        age: 22,
+        budget: 40000
+    },
+    {
+        name: 'Elena',
+        age: 17,
+        budget: 3400
+    },
+    {
+        name: 'Igor',
+        age: 49,
+        budget: 50000
+    },
+    {
+        name: 'Michael',
+        age: 15,
+        budget: 1800
+    },
+    {
+        name: 'Vasilisa',
+        age: 24,
+        budget: 25000
+    },
+    {
+        name: 'Victoria',
+        age: 38,
+        budget: 2300
+    }
+];
+
+// ForEach
+
+// Map
+
+// Filter
+
+// Reduce
+
+// Find
+
+//FindIndex
