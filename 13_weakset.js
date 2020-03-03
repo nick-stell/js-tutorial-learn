5854b3e05ae44fa1b9fbd2346ad10e674b0dae1d
Mykola Stelmakh <nickstelmakh@icloud.com>
2020-03-03 11:59:32 +0200

    13_WeakSet part 4

---
+++ 13_weakset.js
@@ -0,0 +1,19 @@
+const users = [
+  {
+    name: "Elena"
+  },
+  {
+    name: "Ira"
+  },
+  {
+    name: "Mark"
+  }
+];
+
+const visits = new WeakSet();
+visits.add(users[0]).add(users[1]);
+
+users.splice(1, 1);
+
+console.log(visits.has(users[0]));
+console.log(visits.has(users[1]));
